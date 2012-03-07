ef3dd31f61314c12a71b7af65907646e83c9432b
Jon Proulx <jon@csail.mit.edu>
2012-03-07 14:35:53 -0500

    write some tests
    unfortunately without a serial port at /dev/ttyS0 all tests fail,
    though doesn't matter to the test if there's something connected or
    not (if there is that somehting may mind)

--- test/test_alphasign.rb
+++ test/test_alphasign.rb
@@ -1,7 +1,38 @@
 require 'helper'
 
 class TestAlphasign < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
+  def setup
+    @sign=AlphaSign.new()
+    @notsign=AlphaSign.new('/dev/null')
+  end
+
+  def test_simple_write
+    assert_nil( @sign.write("foo" )) # this test *will* fail if you don't have serialport on /dev/ttyS0 
+    exception=assert_raise(ArgumentError) { @notsign.write("foo" ) }
+    assert_equal("not a serial port",exception.message)
+  end
+
+  def test_write_modes
+    AlphaSign::Mode.keys.each do |mode|
+      assert_nil(@sign.write("foo", :mode => mode.to_sym))
+    end
+  end
+
+  def test_write_positions
+    AlphaSign::Position.keys.each do |pos|
+      assert_nil(@sign.write("foo", :position => pos.to_sym))
+    end
+  end
+
+  def test_write_to_file_label
+    assert_nil(@sign.write("foo", :fileLabel=>nil))
+    assert_nil(@sign.write("foo", :fileLabel=>0x20))
+    assert_nil(@sign.write("foo", :fileLabel=>0x29))
+    assert_nil(@sign.write("foo", :fileLabel=>0x31))
+    assert_nil(@sign.write("foo", :fileLabel=>0x75))
+    assert_raise(ArgumentError) {@sign.write("foo", :fileLabel=>"foo")}
+    assert_raise(ArgumentError) {@sign.write("foo", :fileLabel=>0x19)}
+    assert_raise(ArgumentError) {@sign.write("foo", :fileLabel=>0x30)}
+    assert_raise(ArgumentError) {@sign.write("foo", :fileLabel=>0x76)}
   end
 end
