c196a26d691370f7089db2743c276b0a0164ac01
Jon Proulx <jon@jonproulx.com>
2012-03-10 18:30:29 -0500

    pull all file label logic into it's own class

--- lib/alphasign.rb
+++ lib/alphasign.rb
@@ -5,6 +5,7 @@ class AlphaSign
   #We have a relatively large number of potocol and formatting constants
   require 'alphasign/protocol'
   require 'alphasign/format'
+  require 'alphasign/signfile'
   include AlphaSign::Protocol
   include AlphaSign::Format
 
@@ -12,7 +13,13 @@ class AlphaSign
   # @param [String] device the serial device the sign is connected to
   # for now we only speak rs232
   def  initialize (device = "/dev/ttyS0")
-    @device= device
+    @device=SerialPort.new(device, Baud,  DataBits,  StopBits,  Parity) 
+    # default file setup, we should read this from the device, for now
+    # just enforce our own & hope no other process is messing with it
+    # (that's why we're 0.x.x)
+    @files={
+      :default => AlphaSign::SignFile.new(:txt,:fileLabel=>"A")
+    }
 
     # Protocol allows multiple signs on the same port, we are not
     # going to expose that possibility yet but we will recognize this
@@ -24,43 +31,32 @@ class AlphaSign
 
   # we don't have an open yet so this still kludgey and enfoces using
   # only :wtxt command as thats the only one we know we can do @param
-  # @param [STRING] msg the message (text file in Alpha parlance) @see AlphaSign::Format
-  # for control characters for color, font, etc.
+  # @param [STRING] msg the message (text file in Alpha parlance) 
+  # @see AlphaSign::Format for control characters for color, font, etc.
   # @param [HASH] opts the write options
   # @option opts [Symbol] :position display position @see AlphaSign::Format::Position 
   # @options opts [Symbol] :mode display mode @see AlphaSign::Format::Mode
-  # @options opts [Integer] or [Nil] :fileLabel file label to write to
-  # or  nil (for comand modes that don't use it) any value from 0x20
-  # to 0x75 except 0x30 which is reserved
+
   def  write (msg, opts={ })
-    opts[:position]=:middle unless opts[:position] # default to middle position
-    opts[:mode]=:hold unless opts[:mode] #default to static display
-    opts[:fileLabel] =  0x41 unless opts[:fileLabel] #this was default in exampelso why not..
 
-    raise ArgumentError.new("unkown position #{opts[:position]}") unless Position.has_key?(opts[:position])
-    raise ArgumentError.new("unkown mode #{opts[:mode]}") unless Mode.has_key?(opts[:mode])
-    raise ArgumentError.new("invalid File Label specification") unless (opts[:fileLabel] == nil) or  ((opts[:fileLabel].kind_of? Integer) and ( opts[:fileLabel] >= 0x20 and opts[:fileLabel] <= 0x75 and opts[:fileLabel] != 0x30))
-    if opts[:fileLabel] == nil
-      @filelabel=""
-    else
-      @filelabel=[opts[:fileLabel]].pack("C")
-    end
+    # default to middle position
+    opts[:position]=:middle unless opts[:position] 
 
-     @alphaFormat = StartMode + Position[opts[:position]] + Mode[opts[:mode]]
-     @alphaHeader =  StartHeader + @addr +  StartCMD[:wtxt] + @filelabel + @alphaFormat
-     sp=SerialPort.new(@device,  
-                     Baud,  DataBits,  StopBits,  Parity)
-     sp.write @alphaHeader
-     sp.write msg
-     sp.write Footer
-     sp.close
+    #default to static display
+    opts[:mode]=:hold unless opts[:mode] 
+
+    # sign file to write to, carefule these need to be setup before
+    # they can be written to...
+    opts[:filename]=:default unless opts [:filename]
+ 
+    @format = StartMode + Position[opts[:position]] + Mode[opts[:mode]]
+
+    rawwrite  StartCMD[:wtxt]+ @files[opts[:filename]].label + @format + msg 
    end
 
-#raw serial port for testing this will go away (or atleast become private)
+# the most generic write function
+  private
   def rawwrite (msg)
-    sp=SerialPort.new(@device,                                                             
-                      Baud,  DataBits,  StopBits,  Parity) 
-    sp.write msg
-    sp.close
+    @device.write  StartHeader + @addr + msg + Footer
   end
 end
