f253d656549e6553e2d2d4516b0d93af4bc9200b
Jon Proulx <jon@jonproulx.com>
2012-03-10 17:29:22 -0500

    successful writes now return an INT not 'nil'

--- test/test_alphasign.rb
+++ test/test_alphasign.rb
@@ -2,34 +2,42 @@ require 'helper'
 
 class TestAlphasign < Test::Unit::TestCase
   def setup
+    # this test *will* fail if you don't have serialport on /dev/ttyS0 
     @sign=AlphaSign.new()
-    @notsign=AlphaSign.new('/dev/null')
   end
 
   def test_simple_write
-    assert_nil( @sign.write("foo" )) # this test *will* fail if you don't have serialport on /dev/ttyS0 
-    exception=assert_raise(ArgumentError) { @notsign.write("foo" ) }
+    assert_nothing_raised { @sign.write("foo" ) }
+    exception=assert_raise(ArgumentError) {
+      @notsign=AlphaSign.new('/dev/null') 
+    }
     assert_equal("not a serial port",exception.message)
   end
 
   def test_write_modes
-    AlphaSign::Mode.keys.each do |mode|
-      assert_nil(@sign.write("foo", :mode => mode.to_sym))
+    assert_nothing_raised do
+      AlphaSign::Mode.keys.each do |mode|
+        @sign.write("foo", :mode => mode.to_sym)
+      end
     end
   end
 
   def test_write_positions
-    AlphaSign::Position.keys.each do |pos|
-      assert_nil(@sign.write("foo", :position => pos.to_sym))
+    assert_nothing_raised do
+      AlphaSign::Position.keys.each do |pos|
+        @sign.write("foo", :position => pos.to_sym)
+      end
     end
   end
 
   def test_write_to_file_label
-    assert_nil(@sign.write("foo", :fileLabel=>nil))
-    assert_nil(@sign.write("foo", :fileLabel=>0x20))
-    assert_nil(@sign.write("foo", :fileLabel=>0x29))
-    assert_nil(@sign.write("foo", :fileLabel=>0x31))
-    assert_nil(@sign.write("foo", :fileLabel=>0x75))
+    assert_nothing_raised do
+      @sign.write("foo", :fileLabel=>nil)
+      @sign.write("foo", :fileLabel=>0x20)
+      @sign.write("foo", :fileLabel=>0x29)
+      @sign.write("foo", :fileLabel=>0x31)
+      @sign.write("foo", :fileLabel=>0x75)
+    end    
     assert_raise(ArgumentError) {@sign.write("foo", :fileLabel=>"foo")}
     assert_raise(ArgumentError) {@sign.write("foo", :fileLabel=>0x19)}
     assert_raise(ArgumentError) {@sign.write("foo", :fileLabel=>0x30)}
